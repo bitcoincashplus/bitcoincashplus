6d68081297b53ec7dcc912e94eef4396a15c998a
CryptoLeo <leo@zcashplus.org>
2018-01-02 03:57:19 +1100

    Removed unused paramerer

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -77,7 +77,6 @@ struct Params {
     bool fPowAllowMinDifficultyBlocks;
     bool fPowNoRetargeting;
     int64_t nPowTargetSpacing;
-    int64_t nPowTargetTimespan;
     int64_t nPowTargetTimespanLegacy;
 
     int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespanLegacy / nPowTargetSpacing; }
