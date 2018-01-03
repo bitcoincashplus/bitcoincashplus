d955dde6938cd60eb2483e53a76cf40715a2dcf8
CryptoLeo <leo@zcashplus.org>
2018-01-04 05:19:19 +1100

    fixed broken test

--- src/test/net_tests.cpp
+++ src/test/net_tests.cpp
@@ -199,7 +199,7 @@ BOOST_AUTO_TEST_CASE(test_userAgentLength) {
                       "very very very very very very very very very very very "
                       "very very very very very very very very very very very "
                       "very very very very very very very very very very very "
-                      "very very very very very very very ve)/");
+                      "very very very very very very v)/");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
